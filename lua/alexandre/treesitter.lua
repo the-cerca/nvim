3087a4dacdc0c74434a84bb9935287a01aa25031
al <=>
2023-12-11 07:50:22 -0400

    hightlight set to true

--- lua/alexandre/treesitter.lua
+++ lua/alexandre/treesitter.lua
@@ -1,6 +1,10 @@
 require("nvim-treesitter.configs").setup({
 	ensure_installed = { "json", "sql", "lua", "go", "scss", "css", "html", "javascript", "typescript", "svelte" },
 	auto_install = true,
+	highlight = {
+		additional_vim_regex_highlighting = false,
+		enable = true,
+	},
 	indent = {
 		enable = true,
 	},
